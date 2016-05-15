1365c598ab7136117277179f4b0dfe21c86bf981
Matt Robinson <brimstone@the.narro.ws>
2016-05-14 21:19:59 -0400

    Updating test

--- main_test.go
+++ main_test.go
@@ -1,12 +1,34 @@
 package main_test
 
 import (
+	"fmt"
+	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	m "github.com/brimstone/go-le-proxy"
 )
 
-func Test_Proxies(*testing.T) {
-	ts := httptest.NewServer(m.Mux)
+func Test_NewProxyHandler(t *testing.T) {
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Hello, client")
+		fmt.Println(r.Host)
+		fmt.Println(r.RequestURI)
+	}))
+	defer ts.Close()
+
+	url, _ := url.Parse(ts.URL)
+	proxyFunc := m.NewSingleHostReverseProxy("asdf", url)
+	if r, err := http.NewRequest("GET", "/asdf", nil); err != nil {
+		t.Errorf("%v", err)
+	} else {
+		r.Host = "asdf"
+		recorder := httptest.NewRecorder()
+		proxyFunc.ServeHTTP(recorder, r)
+		if recorder.Code != http.StatusOK {
+			t.Errorf("returned %v. Expected %v.", recorder.Code, http.StatusOK)
+		}
+	}
 }
